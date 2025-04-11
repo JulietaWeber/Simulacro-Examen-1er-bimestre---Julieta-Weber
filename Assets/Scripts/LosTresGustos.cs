e4c082bb9c16ec213c0365ea64f9f73b88e5ac98
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2025-04-11 08:45:25 -0300

    Simulacro terminado

---
+++ Assets/Scripts/LosTresGustos.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LosTresGustos : MonoBehaviour
+{
+    public string saborElegido;
+    public float cantidadDeHelado;
+
+    string chocolate = "CHO";
+    string frutilla = "FRU";
+    string dulceDeLeche = "DDL";
+
+    float valorDelGramo = 1.25f;
+    float valorTotal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (saborElegido != "CHO" && saborElegido != "FRU" && saborElegido != "DDL")
+        {
+            Debug.Log("Error");
+            return;
+        }else if (cantidadDeHelado < 250 || cantidadDeHelado > 3000)
+        {
+            Debug.Log("Error");
+            return;
+        }
+
+        if (saborElegido == "CHO" || saborElegido == "DDL")
+        {
+            valorTotal = valorDelGramo * cantidadDeHelado;
+            Debug.Log(valorTotal);
+        }else if (saborElegido == "FRU")
+        {
+            valorTotal = valorDelGramo * (cantidadDeHelado * 0.9f);
+            Debug.Log(valorTotal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
